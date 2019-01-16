fc088b90613291b75fb96b8526d198c24f5de762
Gilbert François Duivesteijn <gilbert@deep-impact.ch>
2019-01-16 12:27:47 +0100

    Code sign in Release mode only

--- cpp/resources/macOS/post_build_codesign.sh
+++ cpp/resources/macOS/post_build_codesign.sh
@@ -2,8 +2,5 @@
 
 APP_BUNDLE_PATH=$1
 
-# Add Qt libraries to the app bundle
-macdeployqt ${APP_BUNDLE_PATH}
-
 # Code sign
 codesign --deep --force --verify --verbose --options runtime --sign "${CODE_SIGN_IDENTITY}" ${APP_BUNDLE_PATH}
